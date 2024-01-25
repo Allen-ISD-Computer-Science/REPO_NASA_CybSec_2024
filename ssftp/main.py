985af2e3f4bfd2d8ef61b4594175094b9553d2af
Richard Jang <jang.richard7017@gmail.com>
2024-01-25 10:35:19 -0600

    basic websocket function started

---
+++ ssftp/main.py
@@ -0,0 +1,37 @@
+import sys
+import argparse
+import socket
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-s', '--send')
+parser.add_argument('-e')
+parser.add_argument('filename')
+
+args = parser.parse_args()
+
+
+def openPort():
+    HOST = None
+    PORT = 3000
+    s = None
+    for res in socket.getaddrinfo(HOST, PORT, socket.AF_UNSPEC, socket.SOCK_STREAM, 0, socket.AI_PASSIVE):
+        af, socktype, proto, canonname, sa = res
+        try:
+            s = socket.socket(af, socktype, proto)
+        except OSError as msg:
+            s = None
+            continue
+        try:
+            s.blind(sa)
+            s.lsiten(1)
+        except OSError as msg:
+            s.close()
+            s = None
+            continue
+        
+
+# Sockets: https://docs.python.org/3/library/socket.html
+# Subprocesses: https://docs.python.org/3/library/subprocess.html
+# Should hash file, filepath, 
+# send and get hash function 
