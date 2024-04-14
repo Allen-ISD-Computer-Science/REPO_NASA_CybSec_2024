eac7f397103842d1ca2d6f18bbc569bdf966f22b
Vansh Bhatt <98234637+theogflamez@users.noreply.github.com>
2024-04-13 23:52:45 -0500

    Update SatelliteTurretNASA.py
    Created Motion tracking software that tracks motion and generated a green rectangle over it.

--- SatelliteTurretNASA.py
+++ SatelliteTurretNASA.py
@@ -3,6 +3,7 @@ import pygame, sys
 import pygame.camera
 from pygame.locals import *
 import os
+import numpy as np
 
 os.environ['XDG_RUNTIME_DIR'] = '/run/user/1000'
 
@@ -14,8 +15,23 @@ screen = pygame.display.set_mode((640, 480))
 cam = pygame.camera.Camera("/dev/video0", (640, 480))
 cam.start()
 
+fgbg = cv2.createBackgroundSubtractorMOG2()
+
 while 1:
 	image = cam.get_image()
+	
+	frame = pygame.surfarray.array3d(image)
+	frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+	fgmask = fgbg.apply(frame)
+	contours, _ = cv2.findContours(fgmask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+	
+	for contour in contours:
+		if cv2.contourArea(contour) > 100:
+			x,  y, w, h = cv2.boundingRect(contour)
+			cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+	
+	frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+	image = pygame.surfarray.make_surface(frame)
 	screen.blit(image,(0, 0))
 	pygame.display.update()
 	for event in pygame.event.get():
